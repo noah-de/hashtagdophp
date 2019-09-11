ab8c2d8e3c2fba7b9e406f0f66a19eac53d01eb8
bminerWestmont <gymbrychamp@gmail.com>
2019-09-11 00:05:38 -0700

    added NOT NULL statements to create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -2,39 +2,39 @@ CREATE TABLE person (
 	student_id	CHAR(8) KEY,
 	social_media_id	SMALLINT KEY,
 	
-	name		VARCHAR(128), --READONLY
-	role		VARCHAR(8), --READONLY
-	year		SMALLINT, --READONLY
-	dorm		CHAR, --READONLY
-	room_num 	CHAR, --READONLY
-	ms_num		SMALLINT, --READONLY
-	phone_num	VARCHAR(24),
-	email		VARCHAR(128),
-	primary_contact	TINYINT, --or VARCHAR(16)
-	times_searched	INT,
-	profile_pic	VARCHAR(256),	
+	name		VARCHAR(128) NOT NULL, --READONLY
+	role		VARCHAR(8) NOT NULL, --READONLY
+	year		SMALLINT NOT NULL, --READONLY
+	dorm		CHAR NOT NULL, --READONLY
+	room_num 	CHAR NOT NULL, --READONLY
+	ms_num		SMALLINT NOT NULL, --READONLY
+	phone_num	VARCHAR(24) NOT NULL,
+	email		VARCHAR(128) NOT NULL,
+	primary_contact	TINYINT DEFAULT 0 NOT NULL, --or VARCHAR(16)
+	times_searched	INT DEFAULT 0 NOT NULL,
+	profile_pic	VARCHAR(256) NOT NULL
 ) PRIMARY KEY (social_media);
 
 CREATE TABLE student_privacy (
 	student_id	CHAR(8) KEY,
 	
-	name		BOOL DEFAULT 1,
-	role		BOOL DEFAULT 1,
-	year		BOOL DEFAULT 0,
-	dorm		BOOL DEFAULT 0,
-	room_num	BOOL DEFAULT 0,
-	ms_num		BOOL DEFAULT 0,
-	social_media	BOOL DEFAULT 0
+	name		BOOL DEFAULT 1 NOT NULL,
+	role		BOOL DEFAULT 1 NOT NULL,
+	year		BOOL DEFAULT 0 NOT NULL,
+	dorm		BOOL DEFAULT 0 NOT NULL,
+	room_num	BOOL DEFAULT 0 NOT NULL,
+	ms_num		BOOL DEFAULT 0 NOT NULL,
+	social_media	BOOL DEFAULT 0 NOT NULL
 ) PRIMARY KEY (social_media);
 
 CREATE TABLE social_media (
 	social_media_id	SMALLINT KEY,
 	
-	stalkernet	BOOL,
-	facebook	VARCHAR(128),
-	instagram	VARCHAR(128),
-	twitter		VARCHAR(128),
-	snapchat	VARCHAR(128),
-	groupme		VARCHAR(128)
+	stalkernet	BOOL NOT NULL,
+	facebook	VARCHAR(128) NOT NULL,
+	instagram	VARCHAR(128) NOT NULL,
+	twitter		VARCHAR(128) NOT NULL,
+	snapchat	VARCHAR(128) NOT NULL,
+	groupme		VARCHAR(128) NOT NULL
 ) PRIMARY KEY (social_media);
 

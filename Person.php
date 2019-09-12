200ac24981bab812c9ad6273bf77767b5a620a21
bminerWestmont <gymbrychamp@gmail.com>
2019-09-12 14:30:25 -0700

    added boilerplate Person classes

---
+++ Person.php
@@ -0,0 +1,118 @@
+<?php
+
+class Person {
+	private $firstname = "";
+	private $lastname = "";
+	private $role = "";
+	private $email = "";
+	private $times_searched = 0;
+
+	function __construct() {
+
+	}
+
+	public function setFirstname ($firstname) {
+		$this->firstname = $firstname;
+	}
+	public function getFirstname () {
+		return $this->firstname;
+	}
+	public function setLastname ($lastname) {
+		$this->lastname = $lastname;
+	}
+	public function getLastname () {
+		return $this->lastname;
+	}
+	public function setRole ($role) {
+		$this->role = $role;
+	}
+	public function getRole () {
+		return $this->role;
+	}
+	public function setEmail ($email) {
+		$this->email = $email;
+	}
+	public function getEmail () {
+		return $this->email;
+	}
+	public function setSearchedNum ($searched_num) {
+		$this->searched_num = $searched_num;
+	}
+	public function getSearchedNum () {
+		return $this->searched_num;
+	}
+}
+
+class Student extends Person {
+	private $student_id = "";
+	private $year = 0;
+	private $dorm = "";
+	private $room_num = "";
+	private $ms_num = 0;
+	private $phone_num = "";
+	private $primary_contact = "";
+	private $roommates = array();
+
+
+	function __construct($student_id) {
+		$this->student_id = $student_id;
+	}
+
+	public function getStudentID () {
+		return $this->student_id;
+	}
+
+	public function setYear ($year) {
+		$this->year = $year;
+	}
+	public function getYear () {
+		return $this->year;
+	}
+	public function setDorm ($dorm) {
+		$this->dorm = $dorm;
+	}
+	public function getDorm () {
+		return $this->dorm;
+	}
+	public function setRoomNum ($room_num) {
+		$this->room_num = $room_num;
+	}
+	public function getRoomNum () {
+		return $this->room_num;
+	}
+	public function setMsNum ($ms_num) {
+		$this->ms_num = $ms_num;
+	}
+	public function getMsNum () {
+		return $this->ms_num;
+	}
+	public function setPhoneNum ($phone_num) {
+		$this->phone_num = $phone_num;
+	}
+	public function getPhoneNum () {
+		return $this->phone_num;
+	}
+	public function setPrimaryContact ($primary_contact) {
+		$this->primary_contact = $primary_contact;
+	}
+	public function getPrimaryContact () {
+		return $this->primary_contact;
+	}
+	// must be passed in as an array
+	public function setRoommates ($roommates) {
+		$this->roommates = $roommates;
+	}
+	public function getRoommates () {
+		return $this->roommates;
+	}
+}
+
+class Faculty extends Person {
+	
+}
+
+class Staff extends Person {
+	
+}
+
+?>
--- create_tables.sql
+++ create_tables.sql
@@ -1,40 +1,53 @@
 CREATE TABLE person (
-	student_id	CHAR(8) KEY,
-	social_media_id	SMALLINT KEY,
+	student_id		CHAR(8) KEY,
 	
-	name		VARCHAR(128) NOT NULL, --READONLY
-	role		VARCHAR(8) NOT NULL, --READONLY
-	year		SMALLINT NOT NULL, --READONLY
-	dorm		CHAR NOT NULL, --READONLY
-	room_num 	CHAR NOT NULL, --READONLY
-	ms_num		SMALLINT NOT NULL, --READONLY
-	phone_num	VARCHAR(24) NOT NULL,
-	email		VARCHAR(128) NOT NULL,
+	firstname		VARCHAR(128) NOT NULL, --READONLY
+	lastname		VARCHAR(128) NOT NULL, --READONLY
+	role			VARCHAR(8) NOT NULL, --READONLY
+	year			SMALLINT NOT NULL, --READONLY
+	dorm			CHAR NOT NULL, --READONLY
+	room_num 		CHAR NOT NULL, --READONLY
+	ms_num			SMALLINT NOT NULL, --READONLY
+	phone_num		VARCHAR(24) NOT NULL,
+	email			VARCHAR(128) NOT NULL,
 	primary_contact	TINYINT DEFAULT 0 NOT NULL, --or VARCHAR(16)
-	times_searched	INT DEFAULT 0 NOT NULL,
-	profile_pic	VARCHAR(256) NOT NULL
-) PRIMARY KEY (social_media);
+	searched_num	INT DEFAULT 0 NOT NULL,
+	profile_pic		VARCHAR(256) NOT NULL
+) UNIQUE (student_id) PRIMARY KEY (student_id);
 
 CREATE TABLE student_privacy (
-	student_id	CHAR(8) KEY,
+	student_id		CHAR(8) KEY,
 	
-	name		BOOL DEFAULT 1 NOT NULL,
-	role		BOOL DEFAULT 1 NOT NULL,
-	year		BOOL DEFAULT 0 NOT NULL,
-	dorm		BOOL DEFAULT 0 NOT NULL,
-	room_num	BOOL DEFAULT 0 NOT NULL,
-	ms_num		BOOL DEFAULT 0 NOT NULL,
-	social_media	BOOL DEFAULT 0 NOT NULL
-) PRIMARY KEY (social_media);
+	name			BOOL DEFAULT 1 NOT NULL,
+	role			BOOL DEFAULT 1 NOT NULL,
+	year			BOOL DEFAULT 0 NOT NULL,
+	dorm			BOOL DEFAULT 0 NOT NULL,
+	room_num		BOOL DEFAULT 0 NOT NULL,
+	ms_num			BOOL DEFAULT 0 NOT NULL,
+	social_media	BOOL DEFAULT 0 NOT NULL,
+	roommates		BOOL DEFAULT 0 NOT NULL
+) UNIQUE (student_id) PRIMARY KEY (student_id);
 
 CREATE TABLE social_media (
-	social_media_id	SMALLINT KEY,
+	student_id		CHAR(8) KEY,
 	
-	stalkernet	BOOL NOT NULL,
-	facebook	VARCHAR(128) NOT NULL,
-	instagram	VARCHAR(128) NOT NULL,
-	twitter		VARCHAR(128) NOT NULL,
-	snapchat	VARCHAR(128) NOT NULL,
-	groupme		VARCHAR(128) NOT NULL
-) PRIMARY KEY (social_media);
+	stalkernet		BOOL NOT NULL,
+	facebook		VARCHAR(128) NOT NULL,
+	instagram		VARCHAR(128) NOT NULL,
+	twitter			VARCHAR(128) NOT NULL,
+	snapchat		VARCHAR(128) NOT NULL,
+	groupme			VARCHAR(128) NOT NULL
+) UNIQUE (student_id) PRIMARY KEY (student_id);
+
+CREATE TABLE roommate (
+	student			CHAR(8),
+	roommate		CHAR(8)
+);
+
+CREATE TABLE user_login (
+	student_id		CHAR(8) KEY,
+	
+	username		VARCHAR(16),
+	hashpass		CHAR(64)
+) UNIQUE (student_id, username, hashpass) PRIMARY KEY (student_id);
 

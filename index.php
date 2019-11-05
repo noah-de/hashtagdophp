3729dd4fd9a75d14e91895d85233635de65e7fb9
Andrew Austin <draustin@westmont.edu>
2019-11-04 17:20:03 -0800

    Testing

--- index.php
+++ index.php
@@ -42,8 +42,8 @@
     <link rel="stylesheet" type="text/css" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="./css/styles.css">
   </head>
-  <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light westmont">
+  <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light westmont">
       <a class="navbar-brand" href="#">
         <img src="./images/westmont.png" height="30" alt="">

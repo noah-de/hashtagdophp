410d266090372a9543d4373ca23db526c141af09
bminerWestmont <gymbrychamp@gmail.com>
2019-11-14 16:38:20 -0800

    fixed merge discrepencies

--- index.php
+++ index.php
@@ -118,77 +118,9 @@ if (count($search_results) > 10) {
   </nav>
   <br>
 
-
-
-<<<<<<< HEAD
- ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-
-    <title>Stalkernet</title>
-
-    <script type="text/javascript" src="./node_modules/jquery/dist/jquery.min.js"></script>
-    <script type="text/javascript" src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <script type="text/javascript" src="./js/script.js"></script>
-
-    <link rel="stylesheet" type="text/css" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="./css/styles.css">
-  </head>
-    
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light westmont">
-      <a class="navbar-brand" href="#">
-        <img src="./images/westmont.png" height="30" alt="">
-      </a>
-      <div class="collapse navbar-collapse" id="navbarNav">
-	<nav class="navbar navbar-expand-lg navbar-light bg-light westmont">
-      </div>
-
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="https://www.westmont.edu/about">ABOUT</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://www.westmont.edu/academics">ACADEMICS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://www.westmont.edu/admissions-aid">ADMISSIONS & AID</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://www.westmont.edu/student-life">STUDENT LIFE</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://www.westmont.edu/giving">GIVING</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://athletics.westmont.edu/index.aspx">ATHLETICS</a>
-          </li>
-	   <li class="nav-item">
-            <a class="nav-link" href="./login/index.php">LOGIN</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-   <br>
-   
-	
-
-
-   <div class="hero-image">
-	 <div class="hero-text" align="center">
-	    <h1>Welcome to Westmont Student Finder</h1>
-	  </div>
-=======
-
   <div class="hero-image">
     <div class="hero-text" align="center">
      <h1>Welcome to Westmont Student Finder</h1>
->>>>>>> 569a5d714c87d4ef423380f4a2af40d4cf6ba80f
    </div>
  </div>
 
